169ef69cb6f6760b788f7917577b00e9d424f44e
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 16:29:10 +0200

    Added detection 2

--- detect.py
+++ detect.py
@@ -9,7 +9,6 @@ import warnings
 
 import cv2
 import imutils
-from imutils.video import VideoStream
 from picamera import PiCamera
 # import the necessary packages
 from picamera.array import PiRGBArray
@@ -26,157 +25,102 @@ warnings.filterwarnings("ignore")
 conf = json.load(open(args["conf"]))
 client = None
 
-# Are we using the Pi Camera?
-usingPiCamera = True
-# Set initial frame size.
-frameSize = (320, 240)
-
 # initialize the camera and grab a reference to the raw camera capture
-# camera = PiCamera()
-# camera.resolution = tuple(conf["resolution"])
-# camera.framerate = conf["fps"]
-# rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
-
-# Initialize mutithreading the video stream.
-vs = VideoStream(src=0, usePiCamera=usingPiCamera, resolution=frameSize,
-                 framerate=32).start()
-
-# Allow the camera to warm up.
-time.sleep(2.0)
-
-timeCheck = time.time()
-while True:
-    # Get the next frame.
-    frame = vs.read()
-
-    # If using a webcam instead of the Pi Camera,
-    # we take the extra step to change frame size.
-    if not usingPiCamera:
-        frame = imutils.resize(frame, width=frameSize[0])
-
-    # Show video stream
-    cv2.imshow('orig', frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the `q` key was pressed, break from the loop.
-    if key == ord("q"):
-        break
-
-    print(1/(time.time() - timeCheck))
-    timeCheck = time.time()
-
-# Cleanup before exit.
-cv2.destroyAllWindows()
-vs.stop()
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 
 # allow the camera to warmup, then initialize the average frame, last
 # uploaded timestamp, and frame motion counter
-# print("[INFO] warming up...")
-# time.sleep(conf["camera_warmup_time"])
-# avg = None
-# lastUploaded = datetime.datetime.now()
-# motionCounter = 0
-#
-# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-# fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+print("[INFO] warming up...")
+time.sleep(conf["camera_warmup_time"])
+avg = None
+lastUploaded = datetime.datetime.now()
+motionCounter = 0
 
-# while True:
-#     ret, frame = vs.read()
-#
-#     fgmask = fgbg.apply(frame)
-#     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-#
-#     cv2.imshow('frame', fgmask)
-#     key = cv2.waitKey(1) & 0xFF
-#     # if the `q` key is pressed, break from the lop
-#     if key == ord("q"):
-#         break
-#
-# cv2.destroyAllWindows()
-# vs.stop()
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image and initialize
+    # the timestamp and occupied/unoccupied text
+    frame = f.array
+    timestamp = datetime.datetime.now()
+    text = "Available"
 
-# # capture frames from the camera
-# for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-#     # grab the raw NumPy array representing the image and initialize
-#     # the timestamp and occupied/unoccupied text
-#     frame = f.array
-#     timestamp = datetime.datetime.now()
-#     text = "Available"
-#
-#     # resize the frame, convert it to grayscale, and blur it
-#     frame = imutils.resize(frame, width=800)
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-#
-#     # if the average frame is None, initialize it
-#     if avg is None:
-#         print("[INFO] starting background model...")
-#         avg = gray.copy().astype("float")
-#         rawCapture.truncate(0)
-#         continue
-#
-#     # accumulate the weighted average between the current frame and
-#     # previous frames, then compute the difference between the current
-#     # frame and running average
-#     cv2.accumulateWeighted(gray, avg, 0.5)
-#     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-#
-#     # threshold the delta image, dilate the thresholded image to fill
-#     # in holes, then find contours on thresholded image
-#     thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
-#                            cv2.THRESH_BINARY)[1]
-#     thresh = cv2.dilate(thresh, None, iterations=2)
-#     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-#                             cv2.CHAIN_APPROX_SIMPLE)
-#     cnts = imutils.grab_contours(cnts)
-#
-#     # loop over the contours
-#     for c in cnts:
-#         # if the contour is too small, ignore it
-#         if cv2.contourArea(c) < conf["min_area"]:
-#             continue
-#
-#         # compute the bounding box for the contour, draw it on the frame,
-#         # and update the text
-#         (x, y, w, h) = cv2.boundingRect(c)
-#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#         text = "Unavailable"
-#
-#     # draw the text and timestamp on the frame
-#     ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-#     cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-#                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-#     cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-#                 0.35, (0, 0, 255), 1)
-#
-#     # check to see if the room is unavailable
-#     if text == "Unavailable":
-#         # check to see if enough time has passed between uploads
-#         if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
-#             # increment the motion counter
-#             motionCounter += 1
-#
-#             # check to see if the number of frames with consistent motion is
-#             # high enough
-#             if motionCounter >= conf["min_motion_frames"]:
-#                 # update the last uploaded timestamp and reset the motion
-#                 # counter
-#                 lastUploaded = timestamp
-#                 motionCounter = 0
-#
-#     # otherwise, the room is not occupied
-#     else:
-#         motionCounter = 0
-#
-#     # check to see if the frames should be displayed to screen
-#     if conf["show_video"]:
-#         # display the roompy window
-#         cv2.imshow("Roompy", frame)
-#         key = cv2.waitKey(1) & 0xFF
-#
-#         # if the `q` key is pressed, break from the lop
-#         if key == ord("q"):
-#             break
-#
-#     # clear the stream in preparation for the next frame
-#     rawCapture.truncate(0)
+    # resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
+
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
+                           cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < conf["min_area"]:
+            continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Unavailable"
+
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.35, (0, 0, 255), 1)
+
+    # check to see if the room is unavailable
+    if text == "Unavailable":
+        # check to see if enough time has passed between uploads
+        if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+            # increment the motion counter
+            motionCounter += 1
+
+            # check to see if the number of frames with consistent motion is
+            # high enough
+            if motionCounter >= conf["min_motion_frames"]:
+                # update the last uploaded timestamp and reset the motion
+                # counter
+                lastUploaded = timestamp
+                motionCounter = 0
+
+    # otherwise, the room is not occupied
+    else:
+        motionCounter = 0
+
+    # check to see if the frames should be displayed to screen
+    if conf["show_video"]:
+        # display the roompy window
+        cv2.imshow("Roompy", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            break
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
