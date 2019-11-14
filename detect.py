b393d2fa25cf742925def8d09e7ee20fa8d95c1e
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 16:07:47 +0200

    Added detection 2

--- detect.py
+++ detect.py
@@ -40,7 +40,7 @@ lastUploaded = datetime.datetime.now()
 motionCounter = 0
 
 
-cap = cv2.VideoCapture(0);
+cap = cv2.VideoCapture(Camera#);
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
