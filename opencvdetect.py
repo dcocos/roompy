126f96f574d55913ec64bd45148b910826cf84d3
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 18:11:04 +0200

    Added detection 2

--- opencvdetect.py
+++ opencvdetect.py
@@ -15,7 +15,11 @@ while True:
 
     n_white_pix = np.sum(fgmask == 255)
 
+    print(n_white_pix)
+
     n_total_pix = fgmask.size
+
+    print(n_total_pix)
 
     if n_white_pix / n_total_pix > 0.1 * n_total_pix:
         print("Movement detected")
