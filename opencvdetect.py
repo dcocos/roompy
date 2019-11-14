71dfc283f9d4bcd13e83cd7c957cf42c4adb954a
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 17:51:42 +0200

    Added detection 2

--- opencvdetect.py
+++ opencvdetect.py
@@ -8,13 +8,6 @@ cv2.startWindowThread()
 
 cap = cv2.VideoCapture(0)
 
-# the output will be written to output.avi
-out = cv2.VideoWriter(
-    'output.avi',
-    cv2.VideoWriter_fourcc(*'MJPG'),
-    15.,
-    (640, 480))
-
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 
@@ -28,8 +21,8 @@ while True:
     # using a greyscale picture, also for faster detection
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    # fgmask = fgbg.apply(frame)
-    # fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
     # detect people in the image
     # returns the bounding boxes for the detected objects
@@ -40,8 +33,6 @@ while True:
         cv2.rectangle(frame, (xA, yA), (xB, yB),
                       (0, 255, 0), 2)
 
-    # Write the output video
-    out.write(frame.astype('uint8'))
     # Display the resulting frame
     cv2.imshow('frame', frame)
 
@@ -52,8 +43,6 @@ while True:
 
 # release the capture
 cap.release()
-# release the output
-out.release()
 # clode the window
 cv2.destroyAllWindows()
 cv2.waitKey(1)
