c12b7a1c554f285d832d91a43203b7383adecdda
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 17:40:17 +0200

    Added detection 2

--- opencvdetect.py
+++ opencvdetect.py
@@ -4,25 +4,63 @@ import json
 import time
 import warnings
 
+import numpy as np
+
 import cv2
 import imutils
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cv2.startWindowThread()
 
 cap = cv2.VideoCapture(0)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 
-while (1):
+# the output will be written to output.avi
+out = cv2.VideoWriter(
+    'output.avi',
+    cv2.VideoWriter_fourcc(*'MJPG'),
+    30.,
+    (1024, 720))
+
+while True:
     ret, frame = cap.read()
 
+    frame = cv2.resize(frame, (640, 480))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
     fgmask = fgbg.apply(frame)
-    # fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=5)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=5)
 
     cv2.imshow('frame', fgmask)
+
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                      (0, 255, 0), 2)
+
+    # Write the output video
+    out.write(frame.astype('uint8'))
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
     key = cv2.waitKey(1) & 0xFF
     # if the `q` key is pressed, break from the lop
     if key == ord("q"):
         break
 
+# release the capture
 cap.release()
+# release the output
+out.release()
+# clode the window
 cv2.destroyAllWindows()
+cv2.waitKey(1)
