4d6c118d124c3fdfb94085e35d338f818c89b94b
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 18:39:59 +0200

    Added detection 3

--- opencvdetect.py
+++ opencvdetect.py
@@ -1,31 +1,40 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+from utils.Event import Event
 
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 
-while True:
-    ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=5)
+class Detector:
 
-    cv2.imshow('frame', fgmask)
+    def __init__(self):
+        self.motionDetected = Event()
+        self.cap = cv2.VideoCapture(0)
+        self.kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        self.fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 
-    n_white_pix = np.sum(fgmask == 255)
-    n_total_pix = fgmask.size
-    white_percent = n_white_pix / float(n_total_pix)
-    threshold = 0.01 * n_total_pix
+    def fire(self):
+        while True:
+            ret, frame = self.cap.read()
+            fgmask = self.fgbg.apply(frame)
+            fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, self.kernel, iterations=5)
 
-    print("n_white_pix: %s , n_total_pix: %s, white_percent: %s, threshold: %s" % (n_white_pix, n_total_pix, white_percent, threshold))
-    if white_percent > threshold:
-        print("Movement detected")
+            cv2.imshow('frame', fgmask)
 
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
+            n_white_pix = np.sum(fgmask == 255)
+            n_total_pix = fgmask.size
+            white_percent = n_white_pix / float(n_total_pix)
+            threshold = 0.02
 
-cap.release()
-cv2.destroyAllWindows()
+            print("n_white_pix: %s , n_total_pix: %s, white_percent: %s, threshold: %s" % (
+                n_white_pix, n_total_pix, white_percent, threshold))
+            if white_percent > threshold:
+                print("Movement detected")
+                self.motionDetected()
+
+            key = cv2.waitKey(1) & 0xFF
+            # if the `q` key is pressed, break from the lop
+            if key == ord("q"):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
