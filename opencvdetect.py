ff6833de66c1c53938fde564ced7212e1ee266cc
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 17:45:41 +0200

    Added detection 2

--- opencvdetect.py
+++ opencvdetect.py
@@ -23,8 +23,8 @@ fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 out = cv2.VideoWriter(
     'output.avi',
     cv2.VideoWriter_fourcc(*'MJPG'),
-    30.,
-    (1024, 720))
+    15.,
+    (640, 480))
 
 while True:
     ret, frame = cap.read()
@@ -34,13 +34,13 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
     fgmask = fgbg.apply(frame)
-    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=5)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
     cv2.imshow('frame', fgmask)
 
     # detect people in the image
     # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
     boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
     for (xA, yA, xB, yB) in boxes:
         # display the detected boxes in the colour picture
