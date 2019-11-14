3fb75fa3e6180179f71073339c40e1923bbc9cb3
Daniel Cocos <daniel.cocos@tradeshift.com>
2019-11-14 16:58:11 +0200

    Added detection 2

---
+++ opencvdetect.py
@@ -0,0 +1,28 @@
+import argparse
+import datetime
+import json
+import time
+import warnings
+
+import cv2
+import imutils
+
+cap = cv2.VideoCapture()
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+fgbg = cv2.createBackgroundSubtractorGMG()
+
+while (1):
+    ret, frame = cap.read()
+
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame', fgmask)
+    key = cv2.waitKey(1) & 0xFF
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
