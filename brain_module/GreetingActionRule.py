b7dd4db5e8e246fe95d07188f1b507eb69f19b24
Adrian Tosca <adrian.tosca@tradeshift.com>
2019-11-15 12:29:19 +0200

    tune

--- brain_module/GreetingActionRule.py
+++ brain_module/GreetingActionRule.py
@@ -11,7 +11,9 @@ class GreetingActionRule:
     def check_do(self):
         brain = self.brain
         for event in brain.current_open_events:
-            if event.start < brain.current_time < event.end and event.id not in self.greeted_events:
+            if event.start < brain.current_time < event.end\
+                    and event.id not in self.greeted_events\
+                    and event.start < brain.last_movement_datetime < event.end:
                 logging.info(f'Movement detected and event not greeted, greeting it.')
                 self.greeted_events[event.id] = True
                 brain.speak.speak('Hello puny human. Thank you for actually showing up for your reservation. '
